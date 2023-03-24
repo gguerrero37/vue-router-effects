892f6eb710036299ebf5083700ed99de5ac28978
gguerrero37 <94309045+gguerrero37@users.noreply.github.com>
2023-03-24 14:08:09 -0300

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <main>
+      <h1>About Page</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate quaerat aut in. Qui, soluta aliquid, aspernatur quos porro reprehenderit consequuntur corrupti, fuga sint beatae dolorem consectetur delectus maxime totam? Qui.</p>
+      <img src="https://picsum.photos/800/300" alt="Placeholder" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, impedit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur labore assumenda repudiandae sint ipsum, eos aliquam quod quaerat magni eius? Dolorum molestias libero laudantium voluptatum?</p>
+    </main>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <main>
+      <h1>Home Page</h1>
+      <img src="https://picsum.photos/800/300" alt="Placeholder" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, impedit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur labore assumenda repudiandae sint ipsum, eos aliquam quod quaerat magni eius? Dolorum molestias libero laudantium voluptatum?</p>
+    </main>
+  </div>
+</template>
